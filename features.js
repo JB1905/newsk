7bbadd2ed64c9aeac7901c89fb86f54ea62cd5a8
Jakub Biesiada <biesiadajakub@icloud.com>
2020-06-20 15:22:05 +0200

    chore: update

--- features.js
+++ features.js
@@ -2,7 +2,7 @@ const featureFlags = {
   pagination: false,
   favorites: false,
   advancedSearch: false,
-  menu: false,
+  menu: true,
 };
 
 export const isFeatureEnabled = (name) => featureFlags[name];
