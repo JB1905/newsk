f24c5850c53a963588c6c3b6e53c2a2c93fcd814
Jakub Biesiada <biesiadajakub@icloud.com>
2020-06-29 17:40:35 +0200

    chore: fix ActionButton on mobile

--- src/components/ActionButton.svelte
+++ src/components/ActionButton.svelte
@@ -6,7 +6,7 @@
   class="mx-2 w-12 h-12 text-gray-700 dark-mode:text-gray-400
   hover:color-gray-400 font-bold p-4 inline-flex items-center appearance-none"
   {name}>
-  <div class="w-5 flex">
+  <div class="w-5 h-5 flex">
     <slot />
   </div>
 </button>
